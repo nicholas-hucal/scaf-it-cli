c2eefd9a42515237e53be82d9bde1b9581fffbea
Nicholas Hucal <nhucal@mac.com>
2025-01-24 17:31:18 -0700

    removed sample files

--- src/components/here/here.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./here.scss";
-
-function here() {
-  return <div className="here">here</div>;
-}
-
-export default here;
--- src/components/herea/herea.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./herea.css";
-
-function herea() {
-  return <div className="herea">herea</div>;
-}
-
-export default herea;
